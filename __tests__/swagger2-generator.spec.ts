6e432ea1dba04adc6d96b1f04e712e3c0c58c00f
jackson <247955056@qq.com>
2021-05-05 14:46:46 +0800

    feat: support put method

--- __tests__/swagger2-generator.spec.ts
+++ __tests__/swagger2-generator.spec.ts
@@ -1,16 +1,107 @@
 import { Swagger2Generator } from '../src/index';
 import { getSwagger } from './data/swagger2';
 
+const PetDef = {
+  "id": {
+    "oneOf": [
+      {
+        "type": "string",
+        allOf: [
+          {
+            maxLength: 5
+          }, {
+            minLength: 2
+          }
+        ]
+      },
+      {
+        type: "number",
+        anyOf: [
+          {
+            multipleOf: 5
+          }, {
+            multipleOf: 2
+          }
+        ],
+        not: {
+          multipleOf: 3
+        }
+      }
+    ]
+  },
+  "category": {
+    "type": "object",
+    "properties": {
+      "id": {
+        "type": "integer",
+        "format": "int64"
+      },
+      "name": {
+        "type": "string"
+      }
+    },
+    "xml": {
+      "name": "Category"
+    }
+  },
+  "name": {
+    "type": "string",
+    "example": "doggie"
+  },
+  "photoUrls": {
+    "type": "array",
+    "xml": {
+      "name": "photoUrl",
+      "wrapped": true
+    },
+    "items": {
+      "type": "string"
+    }
+  },
+  "tags": {
+    "type": "array",
+    "xml": {
+      "name": "tag",
+      "wrapped": true
+    },
+    "items": {
+      "type": "object",
+      "properties": {
+        "id": {
+          "type": "integer",
+          "format": "int64"
+        },
+        "name": {
+          "type": "string"
+        }
+      },
+      "xml": {
+        "name": "Tag"
+      }
+    }
+  },
+  "status": {
+    "type": "string",
+    "description": "pet status in the store",
+    "enum": [
+      "available",
+      "pending",
+      "sold"
+    ]
+  }
+}
+
 
 describe('swagger2-generator', () => {
 
   let generator = new Swagger2Generator(getSwagger())
   let result = generator.generate()
 
-  test('different request method', () => {
+  test('different request method params', () => {
 
     let postRes = result.find(res => res.path === '/pet' && res.method === 'post')
     let getRes = result.find(res => res.path === '/pet/findByStatus' && res.method === 'get')
+    let putRes = result.find(res => res.path === '/pet' && res.method === 'put')
 
     // post
     expect(postRes?.requestSchema).toEqual({
@@ -19,199 +110,9 @@ describe('swagger2-generator', () => {
         "name",
         "photoUrls"
       ],
-      "properties": {
-        "id": {
-          "oneOf": [
-            {
-              "type": "string",
-              allOf: [
-                {
-                  maxLength: 5
-                }, {
-                  minLength: 2
-                }
-              ]
-            },
-            {
-              type: "number",
-              anyOf: [
-                {
-                  multipleOf: 5
-                }, {
-                  multipleOf: 2
-                }
-              ],
-              not: {
-                multipleOf: 3
-              }
-            }
-          ]
-        },
-        "category": {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "integer",
-              "format": "int64"
-            },
-            "name": {
-              "type": "string"
-            }
-          },
-          "xml": {
-            "name": "Category"
-          }
-        },
-        "name": {
-          "type": "string",
-          "example": "doggie"
-        },
-        "photoUrls": {
-          "type": "array",
-          "xml": {
-            "name": "photoUrl",
-            "wrapped": true
-          },
-          "items": {
-            "type": "string"
-          }
-        },
-        "tags": {
-          "type": "array",
-          "xml": {
-            "name": "tag",
-            "wrapped": true
-          },
-          "items": {
-            "type": "object",
-            "properties": {
-              "id": {
-                "type": "integer",
-                "format": "int64"
-              },
-              "name": {
-                "type": "string"
-              }
-            },
-            "xml": {
-              "name": "Tag"
-            }
-          }
-        },
-        "status": {
-          "type": "string",
-          "description": "pet status in the store",
-          "enum": [
-            "available",
-            "pending",
-            "sold"
-          ]
-        }
-      },
+      "properties": PetDef,
       "xml": {
         "name": "Pet"
-      }
-    })
-    expect(postRes?.responseSchema).toEqual({
-      "type": "array",
-      "items": {
-        "type": "object",
-        "required": [
-          "name",
-          "photoUrls"
-        ],
-        "properties": {
-          "id": {
-            "oneOf": [
-              {
-                "type": "string",
-                allOf: [
-                  {
-                    maxLength: 5
-                  }, {
-                    minLength: 2
-                  }
-                ]
-              },
-              {
-                type: "number",
-                anyOf: [
-                  {
-                    multipleOf: 5
-                  }, {
-                    multipleOf: 2
-                  }
-                ],
-                not: {
-                  multipleOf: 3
-                }
-              }
-            ]
-          },
-          "category": {
-            "type": "object",
-            "properties": {
-              "id": {
-                "type": "integer",
-                "format": "int64"
-              },
-              "name": {
-                "type": "string"
-              }
-            },
-            "xml": {
-              "name": "Category"
-            }
-          },
-          "name": {
-            "type": "string",
-            "example": "doggie"
-          },
-          "photoUrls": {
-            "type": "array",
-            "xml": {
-              "name": "photoUrl",
-              "wrapped": true
-            },
-            "items": {
-              "type": "string"
-            }
-          },
-          "tags": {
-            "type": "array",
-            "xml": {
-              "name": "tag",
-              "wrapped": true
-            },
-            "items": {
-              "type": "object",
-              "properties": {
-                "id": {
-                  "type": "integer",
-                  "format": "int64"
-                },
-                "name": {
-                  "type": "string"
-                }
-              },
-              "xml": {
-                "name": "Tag"
-              }
-            }
-          },
-          "status": {
-            "type": "string",
-            "description": "pet status in the store",
-            "enum": [
-              "available",
-              "pending",
-              "sold"
-            ]
-          }
-        },
-        "xml": {
-          "name": "Pet"
-        }
       }
     })
 
@@ -238,7 +139,25 @@ describe('swagger2-generator', () => {
       },
       required: ['status']
     })
-    expect(getRes?.responseSchema).toEqual({
+
+    // put
+    expect(putRes?.requestSchema).toEqual({
+      "type": "object",
+      "required": [
+        "name",
+        "photoUrls"
+      ],
+      "properties": PetDef,
+      "xml": {
+        "name": "Pet"
+      }
+    })
+
+  })
+
+  test('response schema', () => {
+    let postRes = result.find(res => res.path === '/pet' && res.method === 'post')
+    expect(postRes?.responseSchema).toEqual({
       "type": "array",
       "items": {
         "type": "object",
@@ -246,95 +165,7 @@ describe('swagger2-generator', () => {
           "name",
           "photoUrls"
         ],
-        "properties": {
-          "id": {
-            "oneOf": [
-              {
-                "type": "string",
-                allOf: [
-                  {
-                    maxLength: 5
-                  }, {
-                    minLength: 2
-                  }
-                ]
-              },
-              {
-                type: "number",
-                anyOf: [
-                  {
-                    multipleOf: 5
-                  }, {
-                    multipleOf: 2
-                  }
-                ],
-                not: {
-                  multipleOf: 3
-                }
-              }
-            ]
-          },
-          "category": {
-            "type": "object",
-            "properties": {
-              "id": {
-                "type": "integer",
-                "format": "int64"
-              },
-              "name": {
-                "type": "string"
-              }
-            },
-            "xml": {
-              "name": "Category"
-            }
-          },
-          "name": {
-            "type": "string",
-            "example": "doggie"
-          },
-          "photoUrls": {
-            "type": "array",
-            "xml": {
-              "name": "photoUrl",
-              "wrapped": true
-            },
-            "items": {
-              "type": "string"
-            }
-          },
-          "tags": {
-            "type": "array",
-            "xml": {
-              "name": "tag",
-              "wrapped": true
-            },
-            "items": {
-              "type": "object",
-              "properties": {
-                "id": {
-                  "type": "integer",
-                  "format": "int64"
-                },
-                "name": {
-                  "type": "string"
-                }
-              },
-              "xml": {
-                "name": "Tag"
-              }
-            }
-          },
-          "status": {
-            "type": "string",
-            "description": "pet status in the store",
-            "enum": [
-              "available",
-              "pending",
-              "sold"
-            ]
-          }
-        },
+        "properties": PetDef,
         "xml": {
           "name": "Pet"
         }
